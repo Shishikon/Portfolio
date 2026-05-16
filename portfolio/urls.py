d153ce06645937bfcb187c1291820c818ee5cc19
Sherzod <sherzodrazzaqov16@gmail.com>
2026-05-17 00:26:16 +0500

    created serializers, class views, and ready for react

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,4 +5,9 @@ from .tests import *
 urlpatterns = [
     path('', home, name='index'),
 
+    path('api/v1/skills/', SkillListApi.as_view()),
+    path('api/v1/projects/', ProjectListApi.as_view()),
+    path('api/v1/about/', AboutListApi.as_view()),
+    path('api/v1/certifications/', CertificationListApi.as_view()),
+    path('api/v1/education/', EducationListApi.as_view()),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,5 +1,55 @@
+from django.core.serializers import serialize
 from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+
+from .serializers import SkillSerializer, ProjectsSerializer, AboutSerializer, CertificationsSerializer, EducationSerializer
+from .models import Skill, Project, About, Certification, Education
+
+
+
+class SkillListApi(generics.ListAPIView):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+class ProjectListApi(generics.ListAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectsSerializer
+
+class AboutListApi(generics.ListAPIView):
+    queryset = About.objects.all()
+    serializer_class = AboutSerializer
+
+
+class CertificationListApi(generics.ListAPIView):
+    queryset = Certification.objects.all()
+    serializer_class = CertificationsSerializer
+
+
+class EducationListApi(generics.ListAPIView):
+    queryset = Education.objects.all()
+    serializer_class = EducationSerializer
+
+
 
 
 def home(request):
-    return render(request, 'index.html')
+    skills = Skill.objects.all()
+    projects = Project.objects.all()
+    about = About.objects.all()
+    certifications = Certification.objects.all()
+    education = Education.objects.all()
+
+    context = {
+        "skills": skills,
+        "projects": projects,
+        "about": about,
+        "certifications": certifications,
+        "education": education
+    }
+
+
+
+
+    return render(request, 'index.html', context)
