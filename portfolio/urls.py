bf6dc09afd4dcde630fd4838e1cdfe470243271e
Sherzod <sherzodrazzaqov16@gmail.com>
2026-05-18 14:37:48 +0500

    returned to drf without React

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -5,9 +5,9 @@ from .tests import *
 urlpatterns = [
     path('', home, name='index'),
 
-    path('api/v1/skills/', SkillListApi.as_view()),
-    path('api/v1/projects/', ProjectListApi.as_view()),
-    path('api/v1/about/', AboutListApi.as_view()),
-    path('api/v1/certifications/', CertificationListApi.as_view()),
-    path('api/v1/education/', EducationListApi.as_view()),
+    path('api/v1/skills/', SkillListApi.as_view(), name="skills"),
+    path('api/v1/projects/', ProjectListApi.as_view(), name="projects"),
+    path('api/v1/about/', AboutListApi.as_view(), name="about"),
+    path('api/v1/certifications/', CertificationListApi.as_view(), name="certifications"),
+    path('api/v1/education/', EducationListApi.as_view(), name="education"),
 ]
