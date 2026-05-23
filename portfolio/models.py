519d35892e1625488e21a5441be004099cee4d1f
Sherzod <sherzodrazzaqov16@gmail.com>
2026-05-23 22:52:40 +0500

    added desc var for tech model

--- portfolio/models.py
+++ portfolio/models.py
@@ -3,6 +3,7 @@ from django.db import models
 
 class TechStack(models.Model):
     name = models.CharField(max_length=100)
+    description = models.CharField(max_length=150, default='None')
 
     def __str__(self):
         return self.name
